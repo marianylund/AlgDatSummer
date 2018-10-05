8bfe5240e2dabba4ec2a9bc73b0447672cdb1d36
Maria Emine Nylund <mariaemnylund@gmail.com>
2018-10-05 13:58:25 +0200

    Worked a bit on heaps, but the last two elements are stuck in a loop

---
+++ src/dataStructures/Heap.java
@@ -0,0 +1,67 @@
+package dataStructures;
+
+import java.util.Arrays;
+
+public class Heap {
+
+	public static int[] buildMaxHeap(int[] list) {
+		for(int i = (int) Math.floor(list.length/2); i > 0; i--) {
+			System.out.println("We start with number " + list[i]);
+			maxHeapify(list, i);
+		}
+		return list;
+	}
+	
+	public static void maxHeapify(int[] heapList, int index) {
+		
+		int left = getLeftChild(heapList, index);
+		int right = getRightChild(heapList, index);
+		
+
+		
+		int largestIndex = Integer.MIN_VALUE;
+		
+		if(left != -1 && heapList[left] > heapList[index]) {
+			largestIndex = left;
+			System.out.println("Largest is left: " + largestIndex);
+		} else {
+			largestIndex = index;
+			System.out.println("Largest is index: " + largestIndex);
+		}
+		if(right != -1 && heapList[right] > heapList[largestIndex]) {
+			largestIndex = right;
+			System.out.println("Largest is right: " + largestIndex);
+		}
+		if(largestIndex != index) {
+			System.out.println("Largest is not the same as index " + largestIndex + " and " + index);
+			// swap heapList[index] and heapList[largestIndex]
+			int temp = heapList[index];
+			heapList[index] = heapList[largestIndex];
+			heapList[largestIndex] = temp;
+			
+			maxHeapify(heapList, largestIndex);
+		}
+	}
+	
+	public static int getLeftChild(int[] heapList,int nodeIndex) {
+		int leftIndex = 2*heapList[nodeIndex]+1;
+		if(leftIndex > heapList.length) {
+			return -1;
+		}
+		return leftIndex;
+	}
+	
+	public static int getRightChild(int[] heapList, int nodeIndex) {
+		int rightIndex = 2*heapList[nodeIndex]+2;
+		if(rightIndex > heapList.length) {
+			return -1;
+		}
+		return rightIndex;
+	}
+	
+	public static void main(String[] args) {
+		int[] list = Heap.buildMaxHeap(new int[]{7, 0, 5, 6, 8, 3, 4});
+		System.out.println(Arrays.toString(list));
+	}
+	
+}
